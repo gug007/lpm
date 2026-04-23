8ac29482234fd4feb8b0bf5983cde7f8f8fa49b1
gug007 <gurgen.abagyan@gmail.com>
2026-04-23 13:59:55 +0400

    fix(website): remove releases nav link

--- website/components/nav.tsx
+++ website/components/nav.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { AI_AGENTS_PATH, RELEASES_URL, VS_BASE_PATH } from "@/lib/links";
+import { AI_AGENTS_PATH, VS_BASE_PATH } from "@/lib/links";
 import { GitHubStarButton } from "./github-star-button";
 import { NavMobileMenu } from "./nav-mobile-menu";
 import { ThemeToggle } from "./theme-toggle";
@@ -46,9 +46,6 @@ export function Nav() {
           >
             Compare
           </Link>
-          <a href={RELEASES_URL} className={`hidden md:inline ${linkClass}`}>
-            Releases
-          </a>
           <ThemeToggle />
           <NavMobileMenu />
         </div>
